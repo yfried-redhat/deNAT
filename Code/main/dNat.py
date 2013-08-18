ad686530e965fc010ba93a85169113760b572389
yfried <yfried@redhat.com>
2013-08-18 06:30:05 +0300

    created dNat class

---
+++ Code/main/dNat.py
@@ -0,0 +1,19 @@
+'''
+Created on Aug 18, 2013
+
+@author: yfried
+'''
+
+class dNat(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(streams, packets):
+        '''
+        Constructor
+        '''
+        self.streamss = streams
+        self.packets = packets
+        self.hosts = []
